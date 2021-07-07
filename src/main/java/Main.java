832f5ad81f2bbc6914d44cbf9b5bfcba24bb0aad
Tom Dalton <tom.dalton@inovexcorp.com>
2021-07-07 13:08:47 -0600

    Test showing inconsistent getStatements in RDF4j 3+

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,60 @@
-import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.RDFParser;
-import org.eclipse.rdf4j.rio.RDFWriter;
-import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.query.QueryResults;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.sail.nativerdf.NativeStore;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
+import java.util.UUID;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        InputStream stream = Main.class.getResourceAsStream("/test.ttl");
-        StringWriter sw = new StringWriter();
-        RDFParser parser = Rio.createParser(RDFFormat.TURTLE);
-        RDFWriter writer = org.eclipse.rdf4j.rio.Rio.createWriter(RDFFormat.JSONLD, sw);
-        parser.setRDFHandler(writer);
-        parser.parse(stream, "");
-        String val = sw.toString();
-        System.out.println(val);
+        ValueFactory VALUE_FACTORY = SimpleValueFactory.getInstance();
+        IRI subcont = VALUE_FACTORY.createIRI("urn:test");
+        IRI someClass = VALUE_FACTORY.createIRI("urn:someClass");
+
+        NativeStore nativeStore = new NativeStore(new File("target/datadir/" + UUID.randomUUID()));
+        Repository repo = new SailRepository(nativeStore);
+        repo.initialize();
+
+        try (RepositoryConnection conn = repo.getConnection()) {
+            Model startModel = new LinkedHashModel();
+            startModel.add(subcont, RDF.TYPE, someClass);
+            startModel.add(subcont, RDF.TYPE, VALUE_FACTORY.createIRI("urn:someParentClass"));
+
+            conn.add(startModel, subcont);
+            
+            System.out.println("startModel: " + startModel.size());
+            printModel(startModel);
+
+            conn.getStatements(subcont, RDF.TYPE, someClass);
+            
+            conn.clear(subcont);
+            conn.add(startModel, subcont);
+
+            conn.getStatements(subcont, RDF.TYPE, someClass).hasNext();
+
+            Model modelFromContext = QueryResults.asModel(conn.getStatements(null, null, null, subcont));
+            System.out.println("modelFromContext: " + modelFromContext.size());
+            printModel(modelFromContext);
+
+            Model modelFromSubject = QueryResults.asModel(conn.getStatements(subcont, null, null));
+            System.out.println("modelFromSubject: " + modelFromSubject.size());
+            printModel(modelFromSubject);
+        }
+    }
+
+    private static void printModel(Model model) {
+        model.forEach(statement -> {
+            System.out.println("\t\t" + statement.toString());
+        });
     }
 }
