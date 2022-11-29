05978409ffb3fe50a8e388961ebbf98705ff5b3a
Tom Dalton <tom.dalton@inovexcorp.com>
2022-11-29 13:19:55 -0700

    Dataset FedX issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.time.StopWatch;
+import org.eclipse.rdf4j.federated.FedXFactory;
+import org.eclipse.rdf4j.federated.endpoint.Endpoint;
+import org.eclipse.rdf4j.federated.endpoint.EndpointFactory;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Value;
@@ -7,8 +10,10 @@ import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.query.Binding;
 import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.Dataset;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.query.impl.SimpleDataset;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
@@ -22,6 +27,8 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Main {
@@ -47,45 +54,25 @@ public class Main {
             conn.add(model);
             System.out.println("Loading in data COMPLETE");
         }
-        try (RepositoryConnection conn = repo.getConnection()) {
-            TupleQuery query = conn.prepareTupleQuery("prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                    "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-                    "prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
-                    "\n" +
-                    "select ?parent ?child\n" +
-                    "where {\n" +
-                    "    values ?type {owl:Class rdfs:Class}\n" +
-                    "    ?parent rdf:type ?type .\n" +
-                    "    optional {\n" +
-                    "        ?child rdfs:subClassOf ?parent ;\n" +
-                    "               rdf:type ?type .\n" +
-                    "    }\n" +
-                    "}");
-            TupleQueryResult results = query.evaluate();
-            System.out.println("Iterating over query results");
-            StopWatch watch = new StopWatch();
-            watch.start();
-            int parentCount = 0;
-            int childCount = 0;
-            while (results.hasNext()) {
-                BindingSet result = results.next();
-                Value key = Optional.ofNullable(result.getBinding("parent")).orElseThrow(
-                        () -> new RuntimeException("Parent binding must be present for hierarchy")).getValue();
-                Binding value = Optional.ofNullable(result.getBinding("child"))
-                        .orElse(Optional.ofNullable(result.getBinding("individual")).orElse(null));
-                if (value != null) {
-                    parentCount++;
-                    System.out.println(key.stringValue() + "       " + value.getValue().stringValue());
-                } else {
-                    parentCount++;
-                    childCount++;
-                    System.out.println(key.stringValue());
-                }
+        repo.shutDown();
+
+        List<Endpoint> endpoints = new ArrayList<>();
+        endpoints.add(EndpointFactory.loadNativeEndpoint(store.getDataDir()));
+        Repository fedXRepo = FedXFactory.createFederation(endpoints);
+
+        try (RepositoryConnection conn = fedXRepo.getConnection()) {
+            TupleQuery query = conn.prepareTupleQuery("select * where {?s ?p ?o} limit 10");
+            SimpleDataset dataset = new SimpleDataset();
+            dataset.addDefaultGraph(vf.createIRI("http://mobi.com/dataset/d62947b8-b957-49fa-b46a-97c26cf9d7d7_system_dng"));
+            query.setDataset(dataset);
+
+            System.out.println("Dataset: " + query.getDataset());
+
+            // Outputs values not from the dataset
+            TupleQueryResult result = query.evaluate();
+            for (BindingSet solution : result) {
+                System.out.println("?s = " + solution.getValue("s") + "\t" + "?p = " + solution.getValue("p") + "\t" + "?o = " + solution.getValue("o"));
             }
-            System.out.println("Iterating over query results COMPLETE");
-            System.out.println("Total query time: " + watch.getTime());
-            System.out.println("Parent Count: " + parentCount);
-            System.out.println("Child Count: " + childCount);
         }
     }
 }
